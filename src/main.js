fae00b905e6f0367debe9b661c871e14a3df294d
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2024-07-05 14:15:44 +0300

    1111

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import iziToast from 'iziToast';
+import iziToast from 'izitoast';
 import 'iziToast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
