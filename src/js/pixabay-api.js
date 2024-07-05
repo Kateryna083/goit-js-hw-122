cd0e92f2da443bcac7b3c252e6a4c90a294e61ff
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2024-07-05 14:38:37 +0300

    new

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,17 @@
 const apiKey = '44793600-36d61169e013a2e73008f0f0f';
 
-export async function fetchImages(searchQuery) {
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-    searchQuery
-  )}&image_type=photo&orientation=horizontal&safesearch=true`;
+export async function fetchImages(query) {
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error('Failed to fetch images');
     }
     const data = await response.json();
     return data.hits;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching images:', error);
     throw error;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,47 +1,27 @@
-import iziToast from 'izitoast';
-import 'iziToast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './js/pixabay-api.js';
-import {
-  clearGallery,
-  renderImages,
-  displayMessage,
-} from './js/render-functions.js';
+import { renderImages, displayMessage } from './js/render-functions.js';
 
 const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
 const loader = document.querySelector('.loader');
-
-const lightbox = new SimpleLightbox('.gallery a');
 
 searchForm.addEventListener('submit', async event => {
   event.preventDefault();
 
-  const searchInput = document.querySelector('#search-input');
   const searchTerm = searchInput.value.trim();
-
-  if (!searchTerm) {
-    displayMessage('Please enter a search term');
+  if (searchTerm === '') {
+    displayMessage('Please enter a search term.');
     return;
   }
 
-  clearGallery();
-  loader.style.display = 'block';
+  loader.classList.add('visible'); // Відображення індикатора завантаження
 
   try {
     const images = await fetchImages(searchTerm);
-
-    if (images.length === 0) {
-      displayMessage(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    } else {
-      renderImages(images);
-      lightbox.refresh();
-    }
+    renderImages(images);
   } catch (error) {
-    displayMessage('Failed to fetch images. Please try again later.');
+    displayMessage('Failed to fetch images. Please try again.');
   } finally {
-    loader.style.display = 'none';
+    loader.classList.remove('visible'); // Приховання індикатора завантаження
   }
 });
